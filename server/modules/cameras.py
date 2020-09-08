4ae43a4169e5c4e1bc0660f1c08858c4dd9585c5
mpisters <pistersmichelle@gmail.com>
2020-09-08 17:36:01 +0200

    remove error from csv file

--- server/modules/cameras.py
+++ server/modules/cameras.py
@@ -5,13 +5,15 @@ import re
 
 def parse_csv_cameras_to_json(path_to_csv):
     cameras = []
-    print(os.getcwd())
     with open(path_to_csv, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=';')
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
                 line_count += 1
+            match_error = re.match(r'\bERROR\b', row['Camera'])
+            if match_error:
+                continue
             match = re.match(r'^\w*-\w*-(\d*)-?', row['Camera'])
             number = match.group(1) if match else None
             row.update({'number': number})
